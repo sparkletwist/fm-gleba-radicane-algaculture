aec718dabd4880eee0544550f609a3451b4f6514
SafTheLamb <safthelamb@gmail.com>
2026-01-07 20:27:56 -0800

    Water cane with the algacultural bay, in case the player started on Aquilo, I guess?

--- prototypes/compat/seabloom-algaculture.lua
+++ prototypes/compat/seabloom-algaculture.lua
@@ -1,6 +1,7 @@
 if mods["aquilo-seabloom-algaculture"] then
 	local algacultural_bay = data.raw["agricultural-tower"]["algacultural-bay"]
 	if algacultural_bay then
+		table.insert(algacultural_bay.accepted_seeds, "water-cane")
 		table.insert(algacultural_bay.accepted_seeds, "radicane-seed")
 	end
 end
